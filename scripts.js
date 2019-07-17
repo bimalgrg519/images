d28a35421760ce3533adbf1d29eb7df1d976393d
bimal gurung <bimalgrg519@gmail.com>
2019-07-17 16:46:48 +0545

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+{
+    "s": "node node_modules/react-native/local-cli/cli.js start",
+    "R": "adb reverse tcp:8081 tcp:8081",
+    "a": "react-native run-android",
+    "ar": "react-native run-android --variant=release",
+    "i": "react-native run-ios",
+    "r": "adb shell input text 'RR'",
+    "o": "adb shell input keyevent 82"
+}
